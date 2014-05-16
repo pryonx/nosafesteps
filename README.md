4558231b0d587273fd77a0309a10e924c0c7caec
martibn <martibncat@hotmail.com>
2014-05-16 17:45:31 +0200

    single player testing version (no known bugs)

--- README.md
+++
@@ -1,4 +0,0 @@
-nosafesteps
-===========
-
-A JS written Rage Game under the Phaser Framework
---
+++ level/level.js
@@ -0,0 +1,4 @@
+    createSpike(1000,500);
+    createSpike(950,500);
+    createTrap(300,500);
+    createBarrel(520,350);
